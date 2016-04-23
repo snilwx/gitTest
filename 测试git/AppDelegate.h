bd15e09e64826c726fbd7b0b1ada4cbfca8b5055
王翔 <1194163763@qq.com>
2016-04-23 16:17:47 +0800

    Initial Commit

---
+++ 测试git/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  测试git
+//
+//  Created by wangxiang on 16/4/23.
+//  Copyright © 2016年 wx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 测试git/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  测试git
+//
+//  Created by wangxiang on 16/4/23.
+//  Copyright © 2016年 wx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
